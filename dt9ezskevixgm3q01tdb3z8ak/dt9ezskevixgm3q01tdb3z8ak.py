b0d0a41dfdfb564bcee5368fa3c6a4c36ebb32fe
Jevan Goh <javerngoh@gmail.com>
2026-07-05 05:02:22 +0800

    implemented function

--- dt9ezskevixgm3q01tdb3z8ak/dt9ezskevixgm3q01tdb3z8ak.py
+++ dt9ezskevixgm3q01tdb3z8ak/dt9ezskevixgm3q01tdb3z8ak.py
@@ -1,2 +1,27 @@
-def dt9ezskevixgm3q01tdb3z8ak() -> None:
-    pass
+from typing import Optional
+
+from secrets import choice
+from string import ascii_letters, digits
+
+
+def dt9ezskevixgm3q01tdb3z8ak(
+    length: int = 25,
+    available_characters: str = ascii_letters + digits,
+    available_first_characters: Optional[str] = None,
+) -> str:
+    """
+    Generates a random string of specified length using the provided characters. Used for generating the unique IDs of the nodes in Knotos.
+
+    Args:
+        length: The length of the random string to generate.
+        available_characters: The characters to use for generating the random string.
+        available_first_characters: The characters to use for generating the first character of the string. If None, it will be the same as available_characters.
+
+    Returns:
+        A random string.
+    """
+    if available_first_characters is None:
+        return "".join(choice(available_characters) for _ in range(length))
+    return choice(available_first_characters) + "".join(
+        choice(available_characters) for _ in range(length - 1)
+    )
