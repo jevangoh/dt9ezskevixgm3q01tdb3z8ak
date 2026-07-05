e5fcd9cd2db9300b0866bbb709720ee47c5d3a5f
Jevan Goh <javerngoh@gmail.com>
2026-07-05 22:40:13 +0800

    updated template

--- dt9ezskevixgm3q01tdb3z8ak/__init__.py
+++ dt9ezskevixgm3q01tdb3z8ak/__init__.py
@@ -1,3 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
 from .dt9ezskevixgm3q01tdb3z8ak import dt9ezskevixgm3q01tdb3z8ak as _
 
-__all__ = ["_"]
+try:
+    __version__ = version("your_package_distribution_name")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["_", "__version__"]
